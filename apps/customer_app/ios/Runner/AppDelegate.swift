9624ce4f47733538be39a07a9f21e830fa8911ff
ardaaskinn01 <ardaaskindm@gmail.com>
2026-07-21 21:00:00 +0300

    fix: iOS FCM push - bundle bg_alarm2.mp3 in Xcode project.pbxproj, add Runner.entitlements with aps-environment, and update AppDelegate for APNs registration

--- apps/customer_app/ios/Runner/AppDelegate.swift
+++ apps/customer_app/ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import Flutter
 import UIKit
 import GoogleMaps
+import UserNotifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,6 +10,10 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyBKqwiZoUGkmt2ub5ddae3HtTF5ug6ehVs")
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+    application.registerForRemoteNotifications()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
