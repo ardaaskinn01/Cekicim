bea400af87718d286f296df722c4dffca3a2f9da
ardaaskinn01 <ardaaskindm@gmail.com>
2026-07-21 17:59:50 +0300

    fix: iOS build - scope Podfile permission macros; fix debugPrint import in rating_repository; add alarm audio service and APNs headers

--- apps/customer_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/customer_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import agora_rtc_engine
 import app_links
+import audioplayers_darwin
 import connectivity_plus
 import device_info_plus
 import file_selector_macos
@@ -26,6 +27,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AgoraRtcNgPlugin.register(with: registry.registrar(forPlugin: "AgoraRtcNgPlugin"))
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
